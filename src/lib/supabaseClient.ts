4a0acb71f85420f96a047a1b2451510fc480b33d
Marco Hardiman <marco@hardiman.de>
2025-10-04 17:13:12 +0200

    Fix supabaseClient types import

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,5 @@
 "use client";
 
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import type { Database } from "./types"; // falls du noch kein Types-File hast, kannst du diese Zeile entfernen
 
-export const supabase = createClientComponentClient<Database>();
+export const supabase = createClientComponentClient();
